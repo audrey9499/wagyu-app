f3ab644860eb82897c2a30919aae3efdb101ac09
Chris Huang <chrishuang128@gmail.com>
2020-09-23 19:06:28 -0700

    implement styled component to style individual component

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import Icon from '@material-ui/core/Icon';
+import styled from 'styled-components';
 
-export default function Buttons({icon, fontSize, className}) {
-	return <button className={className}>
-		<Icon className="icon" fontSize={fontSize}>{icon}</Icon>
-	</button>;
+const IconButton = styled.button`
+	background: none;
+	border: none;
+	cursor: pointer;
+	&:focus {
+    	outline: none;  
+    }
+`;
+
+export default function Buttons({ icon, fontSize, className }) {
+	return (
+		<IconButton className={className}>
+			<Icon className="icon" fontSize={fontSize}>
+				{icon}
+			</Icon>
+		</IconButton>
+	);
 }
